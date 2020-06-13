44921c65a007b27e3f4e4ce44a5978450fc2b2d3
surya8barca <surya8barca@gmail.com>
2020-06-13 09:46:16 +0530

    Face recognition Dataset.py

--- Face Recognition/Dataset Creation/createdataset.py
+++ Face Recognition/Dataset Creation/createdataset.py
@@ -6,7 +6,8 @@ frames=1;
 face_cascade=cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml');
 name=input('Enter Name:')
 parent_dir=os.getcwd()
-path=os.path.join(parent_dir,name)
+dataset=os.path.join(parent_dir,'Dataset')
+path=os.path.join(dataset,name)
 os.mkdir(path)
 
 while True:
@@ -17,10 +18,6 @@ while True:
     gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 
     faces=face_cascade.detectMultiScale(gray,scaleFactor=1.5,minNeighbors=5)
-    for x,y,w,h in faces:
-        
-        gray=cv2.rectangle(gray,(x,y),(x+w,y+h),(0,255,0),3)
-
     cv2.imshow('Capturing',gray)
 
     if cv2.waitKey(1)==27:

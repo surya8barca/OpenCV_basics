eabf97a6220cb51cbaffbea6b841a491753214b9
surya8barca <surya8barca@gmail.com>
2020-06-13 12:48:35 +0530

    face recognition (webcam)

--- Face Detection/face_detection.py
+++ Face Detection/face_detection.py
@@ -4,24 +4,19 @@ import os
 video = cv2.VideoCapture(0,)
 frames=1;
 face_cascade=cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml');
-name=input('Enter Name:')
-parent_dir=os.getcwd()
-path=os.path.join(parent_dir,name)
-os.mkdir(path)
 
 while True:
     frames+=1;
     check,frame=video.read()
-    cv2.imwrite(os.path.join(path,name+str(frames)+'.jpg'),frame)
 
-    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    #gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 
-    faces=face_cascade.detectMultiScale(gray,scaleFactor=1.5,minNeighbors=5)
+    faces=face_cascade.detectMultiScale(frame,scaleFactor=1.5,minNeighbors=5)
     for x,y,w,h in faces:
         
-        gray=cv2.rectangle(gray,(x,y),(x+w,y+h),(0,255,0),3)
+        gray=cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
 
-    cv2.imshow('Capturing',gray)
+    cv2.imshow('Capturing',frame)
 
     if cv2.waitKey(1)==27:
         break
---
+++ Face Recognition/dataset.py
@@ -0,0 +1,35 @@
+import cv2
+import os
+
+video = cv2.VideoCapture(0)
+video.set(3,640) #breadth
+video.set(4,480) #lenght
+frames=1;
+count=0;
+face_cascade=cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml');
+
+face_id=input('Enter Name of Person:' )
+
+
+
+while True:
+    frames+=1;
+    check,frame=video.read()
+
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+    faces=face_cascade.detectMultiScale(gray,scaleFactor=1.5,minNeighbors=5)
+    for x,y,w,h in faces:
+        
+        gray=cv2.rectangle(gray,(x,y),(x+w,y+h),(0,255,0),3)
+        count+=1;
+        cv2.imwrite("Dataset/User."+str(face_id)+'.'+str(count)+'.jpg',gray[y:y+h,x:x+w])
+        cv2.imshow('Capturing',frame)
+
+    if cv2.waitKey(1)==27 or count==100:
+        break
+
+    
+
+video.release()
+cv2.destroyAllWindows()
